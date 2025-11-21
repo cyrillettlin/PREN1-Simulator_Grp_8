2473a03d4e27d21973f4d9185c217ba96f199835
Laura <lauragrueter97@gmail.com>
2025-11-21 13:12:29 +0100

    add show result function to main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 from edgedetection import EdgeDetection
 import os
+import print_result
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# path = os.path.join(BASE_DIR, "../Data/Real-Puzzle.jpg"
-# path = os.path.join(BASE_DIR, "../Data/best_example.jpg"
-# path = os.path.join(BASE_DIR, "../Data/Real-Puzzle_2.jpg"
-path = os.path.join(BASE_DIR, "../Data/Real-Puzzle_2.jpg")
+path = os.path.join(BASE_DIR, "../Data/Real-Puzzle.jpg")
+# path = os.path.join(BASE_DIR, "../Data/best_example.jpg")
+# path = os.path.join(BASE_DIR, "../Data/Real-Puzzle_2.jpg")
+# path = os.path.join(BASE_DIR, "../Data/Puzzle_1_print.jpg")
 
 detector = EdgeDetection(path)
 detector.load()
 detector.find_contours()
-detector.separate_contours()
-detector.show_result()
+detector.filter_contours()
+print_result.print_result(detector)
+
