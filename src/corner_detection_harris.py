35cb0a1bb2303b8cfb514711b67ad8e80d6d0b8c
Laura <lauragrueter97@gmail.com>
2025-12-07 15:35:51 +0100

    optimize get_best_4_corners

--- src/corner_detection_harris.py
+++ src/corner_detection_harris.py
@@ -15,18 +15,19 @@ img = cv.imread(path)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 gray = np.float32(gray)
-dst = cv.cornerHarris(gray, 4, 3, 0.04)
+dst = cv.cornerHarris(gray, 8, 3, 0.04) #4,3,0.04
 
 # result is dilated for marking the corners, not important
 dst = cv.dilate(dst, None)
 
-# Threshold for an optimal value, it may vary depending on the image.
+# Threshold for an optimal value, it may vary depending on the image, findet lokales maxima
 img[dst > 0.01 * dst.max()] = [0,
  0, 255]
 
 
-cv.namedWindow('dst', cv.WINDOW_NORMAL)
-cv.resizeWindow('dst', 800, 600)
-cv.imshow('dst', img)
+
+
+scaled_output = cv.resize(img, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
+cv.imshow('dst', scaled_output)
 if cv.waitKey(0) & 0xff == 27:
     cv.destroyAllWindows()
